93ef2b8f1eceed570e69028e3b5df9bfcf1e03c5
uzzzrv <144595576+uzzzrv@users.noreply.github.com>
2023-12-18 00:58:00 +0300

    task8.jl

--- Задание8/task8.jl
+++ Задание8/task8.jl
@@ -1,1 +1,45 @@
+using HorizonSideRobots
+robot = Robot(animate = true)
+
+function find_marker!(robot)
+    side = Nord
+    n = 0
+    flag = true 
+    while !find_marker!(robot,side,n)
+        # if flag == true
+        #     n+=1
+        # end
+        if flag
+            n = n + 1
+        end
+        flag  = !flag
+        side = left(side)
+    end
+end
+
+function  find_marker!(robot,side,max_num_steps)
+    for _ in 1:max_num_steps
+        # if ismarker(robot)
+        #     return true
+        # end            
+        ismarker(robot) && return true 
+        move!(robot,side)             
+    end
+    return false            
+end
+
+function move_n!(robot, side::HorizonSide, n)
+    for _ in 1:n
+        move!(robot,side)
+    end    
+end
+
+left(side::HorizonSide) = HorizonSide(mod(Int(side)+1,4))
+
+find_marker!(robot)
+
+
+ 
+
+
 
