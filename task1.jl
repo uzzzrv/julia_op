5535f8e39c91f4b6d147e97136f06ae8c6bd3742
uzzzrv <144595576+uzzzrv@users.noreply.github.com>
2023-12-17 22:29:02 +0300

    task1.jl

---
+++ task1.jl
@@ -0,0 +1,27 @@
+using HorizonSideRobots
+r = Robot("task1.sit", animate = true)
+
+function mark!(r::Robot,side::HorizonSide)
+    while !isborder(r,side)
+        move!(r,side)
+        putmarker!(r)
+    end
+end
+
+function back!(r::Robot,side::HorizonSide)
+    while ismarker(r)
+        move!(r,side)
+    end
+end
+
+function pryamoi_krest!(r::Robot) 
+    for side in (Nord,Sud,West,Ost)
+        mark!(r,side)
+        back!(r,inverse(side))
+    end
+    putmarker!(r)      
+end
+
+inverse(side::HorizonSide) = HorizonSide(mod(Int(side)+2,4))
+
+pryamoi_krest!(r)
