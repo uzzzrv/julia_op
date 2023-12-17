161ba5abba900c107c493753432514e4584b696e
uzzzrv <144595576+uzzzrv@users.noreply.github.com>
2023-12-17 23:55:50 +0300

    task3.jl

--- Задание 3/task3.jl
+++ Задание 3/task3.jl
@@ -1,1 +1,51 @@
+#=
+ДАНО: Робот - в произвольной клетке ограниченного прямоугольного
+поля
+РЕЗУЛЬТАТ: Робот - в исходном положении, и все клетки поля
+промаркированы.
+=#
 
+using HorizonSideRobots
+robot = Robot(animate = true)
+
+function back!(robot,side,num_steps)
+    for x in range(0,num_steps-1)
+        move!(robot,side)
+    end
+end
+inverse(side::HorizonSide) = HorizonSide(mod(Int(side)+2,4))
+
+function totopleft!(robot::Robot,side::HorizonSide)
+    num = 0
+    while !isborder(robot, side)
+        move!(robot,side)
+        num += 1
+    end
+    return num
+end
+
+function mark!(robot::Robot,side::HorizonSide)
+    while !isborder(robot,side)
+        putmarker!(robot)
+        move!(robot,side)
+    end
+    putmarker!(robot)
+end
+
+function vsepole!(robot::Robot)
+    tonord = totopleft!(robot,Nord)
+    towest = totopleft!(robot,West)   
+    side = Ost
+    mark!(robot,side)
+    while !isborder(robot,Sud)
+        move!(robot,Sud)
+        side = inverse(side)
+        mark!(robot,side)
+    end
+    totopleft!(robot,Nord)
+    totopleft!(robot,West)
+    back!(robot,Sud,num_nord)
+    back!(robot,Ost,num_west)
+end
+
+vsepole!(robot)
